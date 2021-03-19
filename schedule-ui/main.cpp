e602bf4db95475c2874ac9af4964caf84db031f0
Anton <anton.lagosha.99@mail.ru>
2021-03-19 20:48:19 +0500

    + add application icon

--- schedule-ui/main.cpp
+++ schedule-ui/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w(std::make_unique<ScheduleDataJsonFile>("data.json"));
+    w.setWindowIcon(QIcon(":/img/calendar.ico"));
     w.show();
 
     return a.exec();
