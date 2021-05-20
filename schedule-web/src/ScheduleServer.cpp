7ee11c4b76880cece9e7e1f3323de0860d588c01
Anton <anton.lagosha.99@mail.ru>
2021-05-20 13:18:17 +0500

    + SSL certificate added

--- schedule-web/src/ScheduleServer.cpp
+++ schedule-web/src/ScheduleServer.cpp
@@ -1,19 +1,19 @@
 #include "ScheduleServer.h"
 #include "ScheduleRequestHandler.h"
 
-#include <Poco/Net/HTTPServer.h>
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/SecureServerSocket.h"
 
 
 int ScheduleServer::main(const std::vector<std::string>&)
 {
     using namespace Poco::Net;
 
-    HTTPServer s(new ScheduleRequestHandlerFactory, ServerSocket(8000), new HTTPServerParams);
+    // set-up a server socket
+    SecureServerSocket svs(8000);
+    HTTPServer s(new ScheduleRequestHandlerFactory, svs, new HTTPServerParams);
     s.start();
-
-    int ch = 0;
-    while((ch = std::getchar()) != 'q');
-
+    waitForTerminationRequest();
     s.stop();
     return Application::EXIT_OK;
 }
