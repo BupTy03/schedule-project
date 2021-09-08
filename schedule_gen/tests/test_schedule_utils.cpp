b77e6a3ee9fe54dcd9bfe0fcdc567363c31aa3e0
BupTy03 <anton.lagosha.99@mail.ru>
2021-09-08 16:17:07 +0500

    ~ using arrays instead vectors

--- schedule_gen/tests/test_schedule_utils.cpp
+++ schedule_gen/tests/test_schedule_utils.cpp
@@ -11,31 +11,31 @@ TEST_CASE("Check if two ordered sets intersects", "[algorithms]")
     }
     SECTION("If one of sets is empty - sets are not intersects")
     {
-        std::vector<int> firstSet = {1, 2, 3};
+        constexpr std::array firstSet = {1, 2, 3};
         std::vector<int> secondSet;
         REQUIRE_FALSE(set_intersects(firstSet, secondSet));
         REQUIRE_FALSE(set_intersects(secondSet, firstSet));
     }
     SECTION("set_intersects function works correctly")
     {
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{1, 2, 3}));
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{2, 3}));
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{1, 2}));
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{1}));
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{2}));
-        REQUIRE(set_intersects(std::vector{1, 2, 3}, std::vector{3}));
-        REQUIRE(set_intersects(std::vector{2, 4, 5, 6, 7, 9}, std::vector{8, 9}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{1, 2, 3}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{2, 3}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{1, 2}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{1}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{2}));
+        REQUIRE(set_intersects(std::array{1, 2, 3}, std::array{3}));
+        REQUIRE(set_intersects(std::array{2, 4, 5, 6, 7, 9}, std::array{8, 9}));
 
-        REQUIRE_FALSE(set_intersects(std::vector{1, 3, 5}, std::vector{2, 4, 6}));
-        REQUIRE_FALSE(set_intersects(std::vector{1, 3, 5}, std::vector{0}));
-        REQUIRE_FALSE(set_intersects(std::vector{1, 3, 5}, std::vector{9, 10}));
+        REQUIRE_FALSE(set_intersects(std::array{1, 3, 5}, std::array{2, 4, 6}));
+        REQUIRE_FALSE(set_intersects(std::array{1, 3, 5}, std::array{0}));
+        REQUIRE_FALSE(set_intersects(std::array{1, 3, 5}, std::array{9, 10}));
     }
 }
 
 TEST_CASE("Check if container contains elements", "[contains]")
 {
-    constexpr std::array<int, 10> arr{4, 2, 3, 6, 1, 8, 10, 43, 22, 21};
-    for(int v : arr)
+    constexpr std::array arr{4, 2, 3, 6, 1, 8, 10, 43, 22, 21};
+    for(auto v : arr)
         REQUIRE(contains(arr, v));
 
     REQUIRE_FALSE(contains(arr, 0));
