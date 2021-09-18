59764341672e8ccf07027c54b7cd9c0b64880341
BupTy03 <anton.lagosha.99@mail.ru>
2021-09-18 21:47:34 +0500

    ~ ChromosomesMutator class

--- schedule_gen/include/ScheduleChromosomes.h
+++ schedule_gen/include/ScheduleChromosomes.h
@@ -71,137 +71,115 @@ ScheduleResult MakeScheduleResult(const ScheduleChromosomes& chromosomes,
                                   const ScheduleData& scheduleData);
 
 
-template<class RandomGenerator>
-void ChangeLessonsBlock(ScheduleChromosomes& chromosomes,
-                        const ScheduleData& data,
-                        const SubjectsBlock& block,
-                        std::size_t requestIndex,
-                        RandomGenerator& randomGenerator)
+class ChromosomesMutator
 {
-    assert(block.Addresses().size() > 1);
+public:
+    explicit ChromosomesMutator(ScheduleChromosomes& chromosomes,
+                                const ScheduleData& data,
+                                std::size_t requestIndex)
+        : mutated_{false}
+        , requestIndex_{requestIndex}
+        , chromosomes_{chromosomes}
+        , data_{data}
+    {}
 
-    if(block.Requests().front() != requestIndex)
-        return;
+    bool Mutated() const { return mutated_; }
 
-    const auto& blockFirstLessons = block.Addresses();
-    const auto& blockRequests = block.Requests();
-
-    std::uniform_int_distribution<std::size_t> lessonsDistrib(0, blockFirstLessons.size() - 1);
-    for(std::size_t tryNum = 0; tryNum < blockFirstLessons.size(); ++tryNum)
+    template<class RandomGenerator>
+    void ChangeLessonsBlock(const SubjectsBlock& block, RandomGenerator& randomGenerator)
     {
-        const std::size_t lesson = blockFirstLessons.at(lessonsDistrib(randomGenerator));
-        bool flag = true;
-        for(std::size_t b = 0; b < blockRequests.size(); ++b)
-        {
-            const std::size_t subjectRequestIndex = blockRequests.at(b);
-            if(chromosomes.GroupsOrProfessorsOrClassroomsIntersects(
-                   data, subjectRequestIndex, lesson + b))
-            {
-                flag = false;
-                break;
-            }
-        }
+        const auto& blockFirstLessons = block.Addresses();
+        const auto& blockRequests = block.Requests();
+        if(block.Addresses().size() <= 1 || blockRequests.front() != requestIndex_)
+            return;
 
-        if(flag)
+        std::uniform_int_distribution<std::size_t> lessonsDistrib(0, blockFirstLessons.size() - 1);
+        for(std::size_t tryNum = 0; tryNum < blockFirstLessons.size(); ++tryNum)
         {
+            const std::size_t lesson = blockFirstLessons.at(lessonsDistrib(randomGenerator));
+            if(lesson == chromosomes_.Lesson(blockRequests.front()))
+                continue;
+
+            bool flag = true;
             for(std::size_t b = 0; b < blockRequests.size(); ++b)
             {
                 const std::size_t subjectRequestIndex = blockRequests.at(b);
-                chromosomes.Lesson(subjectRequestIndex) = lesson + b;
+                if(chromosomes_.GroupsOrProfessorsOrClassroomsIntersects(
+                    data_, subjectRequestIndex, lesson + b))
+                {
+                    flag = false;
+                    break;
+                }
             }
 
+            if(flag)
+            {
+                mutated_ = true;
+                for(std::size_t b = 0; b < blockRequests.size(); ++b)
+                {
+                    const std::size_t subjectRequestIndex = blockRequests.at(b);
+                    chromosomes_.Lesson(subjectRequestIndex) = lesson + b;
+                }
+
+                return;
+            }
+        }
+    }
+
+    template<class RandomGenerator>
+    void ChangeLesson(RandomGenerator& randomGenerator)
+    {
+        const auto& request = data_.SubjectRequests().at(requestIndex_);
+        const auto& lessons = request.Lessons();
+        assert(lessons.size() > 1);
+
+        auto block = data_.FindBlockByRequestIndex(requestIndex_);
+        if(block)
+        {
+            ChangeLessonsBlock(*block, randomGenerator);
             return;
         }
-    }
-}
 
-template<class RandomGenerator>
-void ChangeLesson(ScheduleChromosomes& chromosomes,
-                  const ScheduleData& data,
-                  std::size_t requestIndex,
-                  RandomGenerator& randomGenerator)
-{
-    const auto& request = data.SubjectRequests().at(requestIndex);
-    const auto& lessons = request.Lessons();
-    assert(lessons.size() > 1);
-
-    auto block = data.FindBlockByRequestIndex(requestIndex);
-    if(block)
-    {
-        ChangeLessonsBlock(chromosomes, data, *block, requestIndex, randomGenerator);
-        return;
-    }
-
-    std::uniform_int_distribution<std::size_t> lessonsDistrib(0, lessons.size() - 1);
-    for(std::size_t tryNum = 0; tryNum < lessons.size(); ++tryNum)
-    {
-        const std::size_t lesson = lessons.at(lessonsDistrib(randomGenerator));
-        if(!chromosomes.GroupsOrProfessorsOrClassroomsIntersects(data, requestIndex, lesson))
+        std::uniform_int_distribution<std::size_t> lessonsDistrib(0, lessons.size() - 1);
+        for(std::size_t tryNum = 0; tryNum < lessons.size(); ++tryNum)
         {
-            chromosomes.Lesson(requestIndex) = lesson;
-            return;
+            const std::size_t lesson = lessons.at(lessonsDistrib(randomGenerator));
+            if(!(lesson == chromosomes_.Lesson(requestIndex_) || chromosomes_.GroupsOrProfessorsOrClassroomsIntersects(data_, requestIndex_, lesson)))
+            {
+                mutated_ = true;
+                chromosomes_.Lesson(requestIndex_) = lesson;
+                return;
+            }
         }
     }
-}
 
-template<class RandomGenerator>
-void ChangeClassroom(ScheduleChromosomes& chromosomes,
-                     const ScheduleData& data,
-                     std::size_t requestIndex,
-                     RandomGenerator& randomGenerator)
-{
-    const auto& request = data.SubjectRequests().at(requestIndex);
-    const auto& classrooms = request.Classrooms();
-    assert(classrooms.size() > 1);
-
-    std::uniform_int_distribution<std::size_t> classroomDistrib(0, classrooms.size() - 1);
-    for(std::size_t tryNum = 0; tryNum < classrooms.size(); ++tryNum)
+    template<class RandomGenerator>
+    void ChangeClassroom(RandomGenerator& randomGenerator)
     {
-        const auto scheduleClassroom = classrooms.at(classroomDistrib(randomGenerator));
-        if(!chromosomes.ClassroomsIntersects(chromosomes.Lesson(requestIndex), scheduleClassroom))
+        const auto& request = data_.SubjectRequests().at(requestIndex_);
+        const auto& classrooms = request.Classrooms();
+        assert(classrooms.size() > 1);
+
+        std::uniform_int_distribution<std::size_t> classroomDistrib(0, classrooms.size() - 1);
+        for(std::size_t tryNum = 0; tryNum < classrooms.size(); ++tryNum)
         {
-            chromosomes.Classroom(requestIndex) = scheduleClassroom;
-            return;
+            const auto scheduleClassroom = classrooms.at(classroomDistrib(randomGenerator));
+            if(!(chromosomes_.Classroom(requestIndex_) == scheduleClassroom || chromosomes_.ClassroomsIntersects(chromosomes_.Lesson(requestIndex_), scheduleClassroom)))
+            {
+                mutated_ = true;
+                chromosomes_.Classroom(requestIndex_) = scheduleClassroom;
+                return;
+            }
         }
     }
-}
 
-template<class RandomGenerator>
-void ChangeChromosome(ScheduleChromosomes& chromosomes,
-                      const ScheduleData& data,
-                      std::size_t requestIndex,
-                      RandomGenerator& randomGenerator)
-{
-    const auto& request = data.SubjectRequests().at(requestIndex);
-    const bool canChangeLesson = request.Lessons().size() > 1;
-    const bool canChangeClassroom = request.Classrooms().size() > 1;
-    if(!(canChangeLesson || canChangeClassroom))
-        return;
+private:
+    bool mutated_;
+    std::size_t requestIndex_;
+    ScheduleChromosomes& chromosomes_;
+    const ScheduleData& data_;
+};
 
-    if(canChangeLesson && canChangeClassroom)
-    {
-        std::uniform_int_distribution<std::size_t> headsOrTails(0, 1);
-        if(headsOrTails(randomGenerator))
-        {
-            ChangeLesson(chromosomes, data, requestIndex, randomGenerator);
-        }
-        else
-        {
-            ChangeClassroom(chromosomes, data, requestIndex, randomGenerator);
-        }
-
-        return;
-    }
-
-    if(canChangeLesson)
-    {
-        ChangeLesson(chromosomes, data, requestIndex, randomGenerator);
-    }
-    else
-    {
-        ChangeClassroom(chromosomes, data, requestIndex, randomGenerator);
-    }
-}
 
 template<class RandomGenerator>
 bool Mutate(ScheduleChromosomes& chromosomes,
@@ -211,10 +189,29 @@ bool Mutate(ScheduleChromosomes& chromosomes,
     std::uniform_int_distribution<std::size_t> requestsDistrib(0,
                                                                data.SubjectRequests().size() - 1);
     const auto requestIndex = requestsDistrib(randomGenerator);
-    const auto oldLesson = chromosomes.Lesson(requestIndex);
-    const auto oldClassrooms = chromosomes.Classroom(requestIndex);
-    ChangeChromosome(chromosomes, data, requestIndex, randomGenerator);
+    const auto& request = data.SubjectRequests().at(requestIndex);
+    const bool canChangeLesson = request.Lessons().size() > 1;
+    const bool canChangeClassroom = request.Classrooms().size() > 1;
+    if(!(canChangeLesson || canChangeClassroom))
+        return false;
 
-    return !(oldLesson == chromosomes.Lesson(requestIndex)
-             && oldClassrooms == chromosomes.Classroom(requestIndex));
+    ChromosomesMutator mutator{chromosomes, data, requestIndex};
+    if(canChangeLesson && canChangeClassroom)
+    {
+        std::uniform_int_distribution<std::size_t> headsOrTails{0, 1};
+        if(headsOrTails(randomGenerator))
+            mutator.ChangeLesson(randomGenerator);
+        else
+            mutator.ChangeClassroom(randomGenerator);
+    }
+    else if(canChangeLesson)
+    {
+        mutator.ChangeLesson(randomGenerator);
+    }
+    else
+    {
+        mutator.ChangeClassroom(randomGenerator);
+    }
+
+    return mutator.Mutated();
 }
